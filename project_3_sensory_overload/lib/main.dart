93b8ad1dea0b0ccb0f245e08aaa962fd9bc29157
Josh <thomeczekje@hendrix.edu>
2022-09-20 01:52:52 -0500

    Creating page

--- project_3_sensory_overload/lib/main.dart
+++ project_3_sensory_overload/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Score Page',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,15 +23,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ScorePage(title: 'Scores'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ScorePage extends StatefulWidget {
+  const ScorePage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -45,23 +45,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ScorePage> createState() => _ScorePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _ScorePageState extends State<ScorePage> {
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -97,21 +84,19 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Scores:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ListBody(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+    final List<Score> scores = [
+      const Score(degree: 12),
+      const Score(degree: 20),
+      const Score(degree: 8)
+    ];
   }
 }
 
--- project_3_sensory_overload/lib/scoreboard.dart
+++ project_3_sensory_overload/lib/scoreboard.dart
@@ -3,27 +3,23 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 class Score {
-  const Score(this.degree);
+  const Score({required this.degree});
 
-  final Int degree;
+  final int degree;
 }
 
-class ScoreItem extends StatelessWidget{
-  ScoreItem(
-    {required this.degree
-    required this.onScoreAdded
-    }
-  )
-}
+class ScoreItem extends StatelessWidget {
+  ScoreItem();
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
 
 //not sure if this is necessary?
 //should the entire scoreboard be a class or just a simpler widget
 //with an indefinite number of ScoteItem widgets within?
 
-class Scoreboard extends StatefulWidget{
-  Scoreboard(
-    {required this.score
-  )
-
-}
+class Scoreboard {}
