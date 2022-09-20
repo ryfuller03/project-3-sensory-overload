dce62f21bc38475e2390be63af21aca3a10927ed
Ryan Fuller <fullerra@hendrix.edu>
2022-09-19 22:28:22 -0500

    Layout Done
    The overall layout of this screen is done. Next step: Magnetometer implementation!

--- project_3_sensory_overload/lib/game.dart
+++ project_3_sensory_overload/lib/game.dart
@@ -18,11 +18,7 @@ class Item {
 }
 
 class MyMagnet extends StatefulWidget {
-  const MyMagnet(
-      {super.key,
-      required goalDirectionName,
-      required goalDirectionDegrees,
-      required playerDirectionDegrees});
+  const MyMagnet({super.key});
 
   @override
   State createState() => _MyMagnetState();
@@ -32,21 +28,58 @@ class _MyMagnetState extends State<MyMagnet> {
   Item goalDirection = Item("", 0.0);
   double goalDirectionDegrees = 0.0;
   double playerDirectionDegrees = 0.0;
-  void _setRandomDirection(MyMagnet magnet) {
+
+  void _setRandomDirection() {
     setState(() {
       List directions = [
         Item("North", 0.0),
         Item("South", 180.0),
         Item("East", 90.0),
-        Item("West", 270.0)
+        Item("West", 270.0),
+        Item("Northeast", 45.0),
+        Item("Northwest", 315.0),
+        Item("Southeast", 135.0),
+        Item("Southwest", 225.0)
       ];
-      int randomIndex = Random().nextInt(5);
+      int randomIndex = Random().nextInt(8);
       goalDirection = directions[randomIndex];
     });
   }
 
+  void _ProcessUserAnswer() {
+    double score = (goalDirectionDegrees - playerDirectionDegrees).abs();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Game Screen")));
+    _setRandomDirection();
+    return Scaffold(
+        appBar: AppBar(title: Text("Game Screen")),
+        body: Center(
+            child: Column(
+          children: [
+            Padding(
+              // Goal Direction Text
+              padding: EdgeInsets.only(top: 50.0),
+              child: Text("Goal Direction: ${goalDirection.getName()}",
+                  style: TextStyle(fontSize: 30), textAlign: TextAlign.center),
+            ),
+            const Padding(
+                // Arrow that shows your direction
+                padding: EdgeInsets.only(top: 60.0),
+                child: Icon(Icons.arrow_upward, size: 64.0)),
+            Padding(
+                padding: EdgeInsets.only(top: 300.0),
+                child: ElevatedButton(
+                    child: Text("New Direction"),
+                    onPressed: _setRandomDirection))
+          ],
+        )));
   }
 }
+
+void main() {
+  runApp(const MaterialApp(title: "Magnet Game", home: MyMagnet()));
+}
+
+// TODO: add gyroscope implementation and subtraction and stuff
