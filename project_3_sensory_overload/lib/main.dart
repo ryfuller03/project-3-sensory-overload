df01e5d3f492ae38b867095e3a01b120c2b4f2c6
Ryan Fuller <fullerra@hendrix.edu>
2022-09-23 13:50:35 -0500

    Created Dart File for Scoreboard Rework

--- project_3_sensory_overload/lib/main.dart
+++ project_3_sensory_overload/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_3_sensory_overload/game.dart';
 import 'package:project_3_sensory_overload/scoreboard.dart';
 
 void main() {
@@ -11,13 +12,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Direction Game',
-      home: GameScreen(),
+      title: 'Guess the Direction',
+      home: MyMagnet(),
       //change to game screen when pushed
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+      )
     );
+  }
+}
+
+class ScorePage extends StatefulWidget {
+  const ScorePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
 
