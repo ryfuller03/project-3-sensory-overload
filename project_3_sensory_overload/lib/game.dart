c49ff0d438070f0af6cd129e4891b7f15fdd757e
Ryan Fuller <fullerra@hendrix.edu>
2022-09-23 00:53:23 -0500

    Screen Done!
    I forgot to commit little changes so the entire thing is done now!

--- project_3_sensory_overload/lib/game.dart
+++ project_3_sensory_overload/lib/game.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'package:flutter/services.dart';
 
 class Item {
   final String directionName;
@@ -26,60 +28,128 @@ class MyMagnet extends StatefulWidget {
 
 class _MyMagnetState extends State<MyMagnet> {
   Item goalDirection = Item("", 0.0);
-  double goalDirectionDegrees = 0.0;
-  double playerDirectionDegrees = 0.0;
+  double correctDirection = 0.0;
+  int score = 0;
+  int previousScore = 0;
+  List<double>? _magnetometerValues;
+  bool orangeArrowVisible = false;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+  final Color themeColor = Colors.teal;
+
+  _MyMagnetState() {
+    _setRandomDirection();
+  }
 
   void _setRandomDirection() {
+    List directions = [
+      Item("North", 0.0),
+      Item("South", 180.0),
+      Item("East", 90.0),
+      Item("West", 270.0)
+    ];
+    int randomIndex = Random().nextInt(4);
+    goalDirection = directions[randomIndex];
+    orangeArrowVisible = false;
+  }
+
+  void _showSolution() {
+    orangeArrowVisible = true;
     setState(() {
-      List directions = [
-        Item("North", 0.0),
-        Item("South", 180.0),
-        Item("East", 90.0),
-        Item("West", 270.0),
-        Item("Northeast", 45.0),
-        Item("Northwest", 315.0),
-        Item("Southeast", 135.0),
-        Item("Southwest", 225.0)
-      ];
-      int randomIndex = Random().nextInt(8);
-      goalDirection = directions[randomIndex];
+      previousScore = score;
     });
   }
 
-  void _ProcessUserAnswer() {
-    double score = (goalDirectionDegrees - playerDirectionDegrees).abs();
+  void _processUserAnswer(double x, double y) {
+    // math help from Simon Reid on 09/22/2022
+    setState(() {
+      correctDirection = (atan(y / x) + pi + (pi / 2));
+      correctDirection = x < 0
+          ? correctDirection + pi
+          : correctDirection; // makes it positive // converts from radians to degrees
+      double goalDegreesRadians = goalDirection.getDegrees() * (pi / 180);
+      correctDirection = goalDegreesRadians - correctDirection;
+      double scoreCalculation = (correctDirection * (180 / pi)).toInt() % 360;
+      score = scoreCalculation > 180
+          ? (360 - scoreCalculation).toInt()
+          : scoreCalculation.toInt(); // calculates user score
+      // add score to list
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    _setRandomDirection();
+    final magnetometer =
+        _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+
     return Scaffold(
-        appBar: AppBar(title: Text("Game Screen")),
+        appBar:
+            AppBar(title: Text("Game Screen"), backgroundColor: Colors.teal),
         body: Center(
             child: Column(
           children: [
             Padding(
               // Goal Direction Text
               padding: EdgeInsets.only(top: 50.0),
-              child: Text("Goal Direction: ${goalDirection.getName()}",
+              child: Text("Goal Direction: ${goalDirection.getDegrees()}",
                   style: TextStyle(fontSize: 30), textAlign: TextAlign.center),
             ),
-            const Padding(
-                // Arrow that shows your direction
-                padding: EdgeInsets.only(top: 60.0),
-                child: Icon(Icons.arrow_upward, size: 64.0)),
             Padding(
-                padding: EdgeInsets.only(top: 300.0),
+                padding: EdgeInsets.only(top: 40.0),
+                child: Text("Previous Score: ${previousScore}")),
+            Stack(children: [
+              Icon(Icons.arrow_upward, size: 100.0),
+              orangeArrowVisible == false
+                  ? Icon(Icons.arrow_upward, size: 100.0)
+                  : Transform.rotate(
+                      angle: correctDirection,
+                      child: Icon(Icons.arrow_upward,
+                          size: 100.0, color: Colors.orange))
+            ]),
+            Padding(
+                // Submit Answer button
+                padding: EdgeInsets.only(top: 250.0),
+                child: ElevatedButton(
+                    child: Text("Submit Answer"),
+                    onPressed: _showSolution,
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: themeColor))),
+            Padding(
+                // Gives the player a new direction
+                padding: EdgeInsets.only(top: 20.0),
                 child: ElevatedButton(
                     child: Text("New Direction"),
-                    onPressed: _setRandomDirection))
+                    onPressed: _setRandomDirection,
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: themeColor)))
           ],
         )));
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+    super.dispose();
+  }
+
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
+    _streamSubscriptions.add(magnetometerEvents.listen((event) => {
+          setState(() {
+            _magnetometerValues = <double>[event.x, event.y];
+            _processUserAnswer(event.x, event.y);
+          })
+        }));
   }
 }
 
 void main() {
-  runApp(const MaterialApp(title: "Magnet Game", home: MyMagnet()));
+  runApp(MaterialApp(title: "Magnet Game", home: MyMagnet()));
 }
-
-// TODO: add gyroscope implementation and subtraction and stuff
