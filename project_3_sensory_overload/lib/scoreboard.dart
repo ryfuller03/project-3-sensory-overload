ff325efdc4fa6d3340733077d6a1ef2318e6f682
Josh <thomeczekje@hendrix.edu>
2022-12-11 23:24:59 -0600

    Score screen legibility

--- project_3_sensory_overload/lib/scoreboard.dart
+++ project_3_sensory_overload/lib/scoreboard.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 class ScoreScreen extends StatefulWidget {
   List<int> scoresList;
   List<String> directionList;
-  ScoreScreen({super.key, required this.scoresList, required this.directionList});
+  ScoreScreen(
+      {super.key, required this.scoresList, required this.directionList});
 
   @override
   // ignore: no_logic_in_create_state
@@ -21,22 +22,26 @@ class ScoreScreenState extends State<ScoreScreen> {
   Widget build(BuildContext context) {
     scoresList.sort((a, b) => a.compareTo(b));
     if (scoresList.isNotEmpty) {
-    return Scaffold(
-        appBar: AppBar(
-            title: const Text("Your Scores"), backgroundColor: Colors.teal),
-        body: ListView.builder(
-            key: const Key("Scores List"),
-            itemCount: scoresList.length,
-            prototypeItem: ListTile(title: Text(scoresList.first.toString())),
-            itemBuilder: ((context, index) => ListTile(
-                title: Text(scoresList[index].toString() + ',' + ' ' + directionList[index].toString()),
-                leading: Text((index + 1).toString(),
-                    style: const TextStyle(
-                        color: Colors.orange,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold)),
-                        
-                ))));
+      return Scaffold(
+          appBar: AppBar(
+              title: const Text("Your Scores"), backgroundColor: Colors.teal),
+          body: ListView.builder(
+              key: const Key("Scores List"),
+              itemCount: scoresList.length,
+              prototypeItem: ListTile(title: Text(scoresList.first.toString())),
+              itemBuilder: ((context, index) => ListTile(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.black, width: 1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    title:
+                        Text('${scoresList[index]}, ${directionList[index]}'),
+                    leading: Text((index + 1).toString(),
+                        style: const TextStyle(
+                            color: Colors.orange,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold)),
+                  ))));
     } else {
       return Scaffold(
           appBar: AppBar(
